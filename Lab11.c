31e831d24edd57b86d36fe71c90eeda9478e89f1
Leo Chiu <167877867+E1B06@users.noreply.github.com>
2024-05-16 12:50:33 +0800

    11552 22522

--- Lab11.c
+++ Lab11.c
@@ -1,49 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 void countCharacters();
-int length();
 
-int length(char *ptr) 
-{
-    int count=0;
-
-    while (*ptr!='\0') 
-	{
-        count++;
-        ptr++;
-    }
-
-    return count;
-}
-
-void countCharacters(char *ptr) 
-{
-    int uppercase=0;
-    int lowercase=0;
-    int numbers=0;
-
-    while (*ptr!='\0') 
-	{
-        if (*ptr>='A'&&*ptr<='Z') 
-		{
-            uppercase++;
-        } 
-		else if (*ptr>='a'&&*ptr<='z') 
-		{
-            lowercase++;
-        } 
-		else if (*ptr>='0'&&*ptr<='9') 
-		{
-            numbers++;
-        }
-
-        ptr++;
-    }
-
-    printf("Uppercase letters: %d\n",uppercase);
-    printf("Lowercase letters: %d\n",lowercase);
-    printf("Numbers: %d\n",numbers);
-}
 
 int main(void) 
 {
@@ -52,10 +10,27 @@ int main(void)
     printf("Enter a string: ");
     gets(str);
 
-    int len=length(str);
-    printf("Length of the string: %d\n",len);
+    for(int i=0;i<=SIZE-1;i++)
+    {
+        if(i==1)
+        {
+            printf("%s",str[i]);
+            break;
+        }
+        else if(i>=1&&i<SIZE-1)
+        {
+            printf("*");
+            break;
+        }
+        else if(i==SIZE-1)
+        {
+            printf("%s",str[i]);
+            break;
+        }
 
-    countCharacters(str);
+
+
+    }
 
     system("PAUSE");
     return 0;
