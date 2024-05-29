8528f7aacf226022d86fbd7fb1cf1dce4755c181
Leo Chiu <167877867+E1B06@users.noreply.github.com>
2024-05-30 00:17:04 +0800

    完成a部分 這一題要怎麼判斷學號為6位整數困擾了我很久，後來想起程式設計有教過strlen的函數，才把問題解決

---
+++ HW#4.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void star(void)
+{
+	printf("*********************\n");
+	return;
+}
+
+void Main(void)
+{
+	printf("----------[Grade System]----------\n");
+	printf("* a. Enter student grades        *\n");
+	printf("* b. Display student's grades    *\n");
+	printf("* C. Search for student's grades *\n");
+	printf("* d. Grade ranking               *\n");
+	printf("* d. Exit                        *\n");
+	printf("----------------------------------\n");	
+	return;
+}
+
+void HW(void)
+{
+	star();//呼叫star函數 
+	printf("* 歡迎來到程式作業4 *\n");
+	printf("* 按任意鍵以開始遊戲*\n");  
+	star();//呼叫star函數
+	printf("\n");
+	puts("H       H       A       PPPPP   PPPPP   Y       Y   ");
+	puts("H       H      A A      P    P  P    P   Y     Y    "); 
+	puts("H       H     A   A     P     P P     P   Y   Y     ");
+	puts("HHHHHHHHH    A     A    P    P  P    P     Y Y      ");
+	puts("H       H   AAAAAAAAA   PPPPP   PPPPP       Y       ");
+	puts("H       H  A         A  P       P           Y       ");
+	puts("H       H A           A P       P           Y       ");
+	puts("Dragon Boat Festival");
+	return;
+}
+struct information
+{
+	char name[25];
+	char ID[6];
+	int math;
+	int phy;
+	int eng;
+}; 
+
+int main(void)
+{
+	HW(); 
+	getch();
+	system("cls");//清除螢幕
+	int a,b=2;
+	float avg[10];
+	printf("請輸入4位數的密碼:");
+	scanf("%d",&a);
+	while(a!=2024)//密碼=2024 
+	{
+		if(b==0)//當b=0時，結束程式 
+		{
+			printf("三次機會已用完，掰掰");
+			exit(EXIT_FAILURE);
+		}
+		printf("輸入錯誤，還剩%d次機會:",b);
+		scanf("%d",&a);
+		b=b-1;
+	}
+	system("cls");//清除螢幕
+	while(1)
+	{
+		Main();//呼叫Main函數，把主菜單叫出來
+		int n,c;
+		printf("請輸入:");
+		scanf("%s",&c);		
+		switch(c)
+		{
+		case 'A':
+		case 'a':
+			system("cls");//清除螢幕
+			struct information student[10];
+			printf("請輸入n(5-10):");
+			fflush(stdin);
+			scanf("%d",&n);
+			
+			for(int i=1;i<=n;i++)
+			{
+				printf("請分別輸入%d號學生的姓名、學號、數學成績、物理成績、英文成績\n",i);
+				scanf("%s %s %d %d %d",&student[i].name,&student[i].ID,&student[i].math,&student[i].phy,&student[i].eng);
+				while(strlen(student[i].ID)!=6)
+				{
+					printf("學號輸入錯誤，請重新輸入學號:");
+					fflush(stdin);
+					scanf("%s",&student[i].ID);
+				}
+				while(student[i].math<0||student[i].math>100)
+				{
+					printf("數學成績輸入錯誤，請重新輸入數學成績:");
+					fflush(stdin);
+					scanf("%d",&student[i].math);
+				}
+				while(student[i].phy<0||student[i].phy>100)
+				{
+					printf("物理成績輸入錯誤，請重新輸入物理成績:");
+					fflush(stdin);
+					scanf("%d",&student[i].phy);
+				}
+				while(student[i].eng<0||student[i].eng>100)
+				{
+					printf("英文成績輸入錯誤，請重新輸入物理英文成績:");
+					fflush(stdin);
+					scanf("%d",&student[i].eng);
+				}
+				avg[i]=((float)(student[i].math+student[i].phy+student[i].eng))/3;
+				printf("%3.1f",avg[i]);
+			}
+			printf("按任意鍵以回到主選單");
+			getch();
+			system("cls");//清除螢幕
+			break;
+		case 'B':
+		case 'b':
+			system("cls");
+			printf("姓名\t學號\tMath\tPhysics\tEnglish\t平均成績");
+			for(int j=1;j<=n;j++)
+			{
+				printf("\n%s\t%s\t%d\t%d\t%d\t%3.1f\n",student[j].name,student[j].ID,student[j].math,student[j].phy,student[j].eng,avg[j]);
+			}
+			getch();
+			system("cls");//清除螢幕
+			break;
+		case 'E':
+		case 'e':
+				system("cls");//清除螢幕
+				char f;
+				printf("Continue? (y/n):");
+				scanf("%s",&f);
+				while(f!='Y'&&f!='y'&&f!='N'&&f!='n')
+				{
+					printf("Continue? (y/n):");
+					scanf("%s",&f);
+				}
+				switch(f)
+				{
+					case 'Y':
+					case 'y':
+						system("cls");//清除螢幕
+						break;
+					case 'N':
+					case 'n':
+						printf("按任意鍵以結束程式");
+						exit(EXIT_FAILURE);	
+				}
+				break;	
+		}
+	}
+    return 0;
+} 
