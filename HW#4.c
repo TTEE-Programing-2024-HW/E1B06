825b0bc5bcd566e104509e3c8ae7555b52f75d18
Leo Chiu <167877867+E1B06@users.noreply.github.com>
2024-05-31 01:00:40 +0800

    剛剛看到作業限制(主選單的a-d項目請各以函式實現)，所以重新更正過

--- HW#4.c
+++ HW#4.c
@@ -44,7 +44,10 @@ struct information
 	int math;
 	int phy;
 	int eng;
-}; 
+};
+struct information student[10]; 
+float avg[10];
+int num;
 
 int main(void)
 {
@@ -52,7 +55,6 @@ int main(void)
 	getch();
 	system("cls");//清除螢幕
 	int a,b=2;
-	float avg[10];
 	printf("請輸入4位數的密碼:");
 	scanf("%d",&a);
 	while(a!=2024)//密碼=2024 
@@ -70,119 +72,26 @@ int main(void)
 	while(1)
 	{
 		Main();//呼叫Main函數，把主菜單叫出來
-		int n;
 		char c;
-		struct information student[10];
 		printf("請輸入:");
 		scanf(" %c",&c);		
 		switch(c)
 		{
 		case 'A':
 		case 'a':
-			system("cls");//清除螢幕
-			printf("請輸入n(5-10):");
-			fflush(stdin);
-			scanf("%d",&n);
-			while(n<5||n>10)
-			{
-				printf("輸入錯誤，請重新輸入n(5-10):");
-				fflush(stdin);
-				scanf("%d",&n);
-			}
-			for(int i=1;i<=n;i++)
-			{
-				printf("請分別輸入%d號學生的姓名、學號、數學成績、物理成績、英文成績\n",i);
-				scanf("%s %s %d %d %d",&student[i].name,&student[i].ID,&student[i].math,&student[i].phy,&student[i].eng);
-				for(int l=1;l<=n;l++)//檢查姓名是否重複 
-				{
-					while(strcmp(student[l].name,student[i].name)==0&&l!=i)
-					{
-						printf("姓名重複，請重新輸入姓名:");
-						fflush(stdin);
-						scanf("%s",&student[i].name);
-					}
-				}
-				while(strlen(student[i].ID)!=6)//檢查學號格式是否正確 
-				{
-					printf("學號錯誤，請重新輸入學號:");
-					fflush(stdin);
-					scanf("%s",&student[i].ID);
-				}
-				for(int t=1;t<=n;t++)//檢查學號是否重複 
-				{
-					while(strcmp(student[t].ID,student[i].ID)==0&&t!=i)
-					{
-						printf("學號重複，請重新輸入學號:");
-						fflush(stdin);
-						scanf("%s",&student[i].ID);
-					}
-				}
-				while(student[i].math<0||student[i].math>100)//檢查數學成績是否正確 
-				{
-					printf("數學成績錯誤，請重新輸入數學成績:");
-					fflush(stdin);
-					scanf("%d",&student[i].math);
-				}
-				while(student[i].phy<0||student[i].phy>100)//檢查物理成績是否正確 
-				{
-					printf("物理成績錯誤，請重新輸入物理成績:");
-					fflush(stdin);
-					scanf("%d",&student[i].phy);
-				}
-				while(student[i].eng<0||student[i].eng>100)//檢查英文成績是否正確 
-				{
-					printf("英文成績錯誤，請重新輸入英文成績:");
-					fflush(stdin);
-					scanf("%d",&student[i].eng);
-				}
-			}
-			printf("按任意鍵以回到主選單");
-			getch();
-			system("cls");//清除螢幕
+			afun();
 			break;
 		case 'B':
 		case 'b':
-			system("cls");
-			printf("姓名\t學號\tMath\tPhysics\tEnglish\t平均成績");
-			for(int j=1;j<=n;j++)
-			{
-				avg[j]=((float)(student[j].math+student[j].phy+student[j].eng))/3;//計算平均 
-				printf("\n%s\t%s\t%d\t%d\t%d\t%3.1f\n",student[j].name,student[j].ID,student[j].math,student[j].phy,student[j].eng,avg[j]);
-			}
-			printf("\n");
-			printf("按任意鍵以回到主選單");
-			getch();
-			system("cls");//清除螢幕
+			bfun();
 			break;
 		case 'C':
 		case 'c':
-			system("cls");
-			int num=0;
-			char name1[25];
-			printf("請輸入想要搜尋的姓名:");
-			fflush(stdin);
-			scanf("%s",&name1);	
-			for(int k=1;k<=n;k++)
-			{
-				if(strcmp(student[k].name,name1)==0)
-				{
-					num=k;
-					break;
-				}
-			}
-			if(num==0)
-			{
-				printf("找不到此人");
-			}
-			else
-			{
-				printf("姓名\t學號\tMath\tPhysics\tEnglish\t平均成績");	
-				printf("\n%s\t%s\t%d\t%d\t%d\t%3.1f\n",student[num].name,student[num].ID,student[num].math,student[num].phy,student[num].eng,avg[num]);
-			}
-			printf("\n");
-			printf("按任意鍵以回到主選單");
-			getch();
-			system("cls");//清除螢幕
+			cfun();
+			break;
+		case 'D':
+		case 'd':
+			dfun();
 			break;
 		case 'E':
 		case 'e':
@@ -211,3 +120,120 @@ int main(void)
 	}
     return 0;
 } 
+void afun(void)
+{
+	system("cls");//清除螢幕
+	int n;
+	printf("請輸入n(5-10):");
+	fflush(stdin);
+	scanf("%d",&n);
+	num=n;
+	while(n<5||n>10)
+	{
+		printf("輸入錯誤，請重新輸入n(5-10):");
+		fflush(stdin);
+		scanf("%d",&n);
+	}
+	for(int i=1;i<=n;i++)
+	{
+		printf("請分別輸入%d號學生的姓名、學號、數學成績、物理成績、英文成績\n",i);
+		scanf("%s %s %d %d %d",&student[i].name,&student[i].ID,&student[i].math,&student[i].phy,&student[i].eng);
+		for(int l=1;l<=n;l++)//檢查姓名是否重複 
+		{
+			while(strcmp(student[l].name,student[i].name)==0&&l!=i)
+			{
+				printf("姓名重複，請重新輸入姓名:");
+				fflush(stdin);
+				scanf("%s",&student[i].name);
+			}
+		}
+		for(int t=1;t<=n;t++)//檢查學號是否重複 
+		{
+			while(strcmp(student[t].ID,student[i].ID)==0&&t!=i)
+			{
+				printf("學號重複，請重新輸入學號:");
+				fflush(stdin);
+				scanf("%s",&student[i].ID);
+			}
+		}
+		while(strlen(student[i].ID)!=6)//檢查學號格式是否正確 
+		{
+			printf("學號錯誤，請重新輸入學號:");
+			fflush(stdin);
+			scanf("%s",&student[i].ID);
+		}
+		while(student[i].math<0||student[i].math>100)//檢查數學成績是否正確 
+		{
+			printf("數學成績錯誤，請重新輸入數學成績:");
+			fflush(stdin);
+			scanf("%d",&student[i].math);
+		}
+		while(student[i].phy<0||student[i].phy>100)//檢查物理成績是否正確 
+		{
+			printf("物理成績錯誤，請重新輸入物理成績:");
+			fflush(stdin);
+			scanf("%d",&student[i].phy);
+		}
+		while(student[i].eng<0||student[i].eng>100)//檢查英文成績是否正確 
+		{
+			printf("英文成績錯誤，請重新輸入英文成績:");
+			fflush(stdin);
+			scanf("%d",&student[i].eng);
+		}
+	}
+	printf("按任意鍵以回到主選單");
+	getch();
+	system("cls");//清除螢幕
+	return;
+}
+void bfun(void)
+{
+	system("cls");
+	printf("姓名\t學號\tMath\tPhysics\tEnglish\t平均成績");
+	for(int j=1;j<=num;j++)
+	{
+		avg[j]=((float)(student[j].math+student[j].phy+student[j].eng))/3;//計算平均 
+		printf("\n%s\t%s\t%d\t%d\t%d\t%3.1f\n",student[j].name,student[j].ID,student[j].math,student[j].phy,student[j].eng,avg[j]);
+	}
+	printf("\n");
+	printf("按任意鍵以回到主選單");
+	getch();
+	system("cls");//清除螢幕
+	return;
+}
+void cfun(void)
+{
+	system("cls");
+	int num1=0;
+	char name2[25];
+	printf("請輸入想要搜尋的姓名:");
+	fflush(stdin);
+	scanf("%s",&name2);	
+	for(int k=1;k<=num;k++)
+	{
+		if(strcmp(student[k].name,name2)==0)
+		{
+			num1=k;
+			break;
+		}
+	}
+	if(num1==0)
+	{
+		printf("找不到此人");
+	}
+	else
+	{
+		printf("姓名\t學號\tMath\tPhysics\tEnglish\t平均成績");	
+		printf("\n%s\t%s\t%d\t%d\t%d\t%3.1f\n",student[num1].name,student[num1].ID,student[num1].math,student[num1].phy,student[num1].eng,avg[num1]);
+	}
+	printf("\n");
+	printf("按任意鍵以回到主選單");
+	getch();
+	system("cls");//清除螢幕
+	return;	
+}
+void dfun(void)
+{
+	
+}
+
